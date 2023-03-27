0480d9d75b66bb883cf90c1432794eecba78bb7e
ldkhoa10 <94123607+ldkhoa10@users.noreply.github.com>
2023-03-27 21:08:38 +0700

    Siuuuuu

--- hotel management/Hotel Management System/src/com/hotelManagementSystem/views/SearchRoom.java
+++ hotel management/Hotel Management System/src/com/hotelManagementSystem/views/SearchRoom.java
@@ -62,25 +62,25 @@ public class SearchRoom extends JFrame {
         p1 = new JPanel();
         p1.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(p1);
-        p1.setSize(1000,600);
+        p1.setSize(1100,600);
         p1.setLayout(null);
         room = new Room();
 
         JTextArea lblSearchForRoom = new JTextArea("Search For Room");
         lblSearchForRoom.setFont(new Font("serif", Font.BOLD, 30));
-        lblSearchForRoom.setBounds(0, 10, 700, 50);
+        lblSearchForRoom.setBounds(0, 10, 1100, 50);
         lblSearchForRoom.setForeground(Color.decode("#e09145"));
         lblSearchForRoom.setBackground(Color.decode("#1b1c22"));
         lblSearchForRoom.setEditable(false);
         p1.add(lblSearchForRoom);
 
         JLabel lblRoomAvailable = new JLabel("Room Bed Type:");
-        lblRoomAvailable.setBounds(10, 70, 95, 14);
+        lblRoomAvailable.setBounds(10, 100, 100, 27);
         p1.add(lblRoomAvailable);
 
 
         comboBox = new JComboBox(new String[] { "Single Bed", "Double Bed", "King bed"});
-        comboBox.setBounds(105, 68, 120, 20);
+        comboBox.setBounds(130, 100, 150, 20);
         comboBox.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -90,12 +90,12 @@ public class SearchRoom extends JFrame {
         p1.add(comboBox);
 
         JLabel lblAvailable = new JLabel("Availability:");
-        lblAvailable.setBounds(400, 70, 95, 14);
+        lblAvailable.setBounds(400, 100, 100, 27);
         //lblRoomAvailable.setFont(new Font("Arial", Font.PLAIN, 14));
         p1.add(lblAvailable);
 
         comboBox_1 = new JComboBox(new String[] { "all", "available", "occupied"});
-        comboBox_1.setBounds(500, 69, 120, 20);
+        comboBox_1.setBounds(510, 100, 150, 20);
         comboBox_1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -107,39 +107,39 @@ public class SearchRoom extends JFrame {
 
 
         JLabel lblRoomType = new JLabel("Room Number");
-        lblRoomType.setBounds(23, 100, 96, 14);
+        lblRoomType.setBounds(70, 150, 150, 27);
         p1.add(lblRoomType);
 
         JLabel lblRoomAvailable_1 = new JLabel("Availability");
-        lblRoomAvailable_1.setBounds(175, 100, 120, 14);
+        lblRoomAvailable_1.setBounds(270, 150, 150, 27);
         p1.add(lblRoomAvailable_1);
 
+        JLabel lblCleanStatus = new JLabel("Clean Status");
+        lblCleanStatus.setBounds(470, 150, 150, 27);
+        p1.add(lblCleanStatus);
+
         JLabel lblPrice_1 = new JLabel("Price");
-        lblPrice_1.setBounds(458, 100, 46, 14);
+        lblPrice_1.setBounds(670, 150, 150, 27);
         p1.add(lblPrice_1);
 
         JLabel l1 = new JLabel("Bed Type");
-        l1.setBounds(580, 100, 96, 14);
+        l1.setBounds(870, 150, 150, 27);
         p1.add(l1);
 
 
 
         table = new JTable();
+        table.setBounds(50, 190, 1000, 400);
+        table.setForeground(Color.WHITE);
+        table.setBackground(Color.decode("#292c35"));
+        p1.add(table);
+
         JButton btnSearch = new JButton("Search");
-        btnSearch.setBounds(200, 400, 120, 30);
+        btnSearch.setBounds(900, 85, 150, 50);
         btnSearch.setBackground(Color.BLACK);
         btnSearch.setForeground(Color.WHITE);
         new SearchRoomController().searchRoom(btnSearch, table, room);
         p1.add(btnSearch);
-
-        table.setBounds(0, 120, 700, 300);
-        table.setBackground(Color.BLUE);
-        p1.add(table);
-
-
-        JLabel lblCleanStatus = new JLabel("Clean Status");
-        lblCleanStatus.setBounds(306, 100, 96, 14);
-        p1.add(lblCleanStatus);
 
         getContentPane().setBackground(Color.WHITE);
     }
