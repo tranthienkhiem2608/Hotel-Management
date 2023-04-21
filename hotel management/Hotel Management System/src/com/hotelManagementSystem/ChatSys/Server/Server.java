dab8d067ced8f1ec4d1fe06bbf2eb4ac8eed91f7
lực lê <11lucviet@gmail.com>
2023-04-21 09:05:13 +0700

    update chat

---
+++ hotel management/Hotel Management System/src/com/hotelManagementSystem/ChatSys/Server/Server.java
@@ -0,0 +1,79 @@
+package com.hotelManagementSystem.ChatSys.Server;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author Admin
+ */
+public class Server {
+
+    public static volatile ServerThreadBus serverThreadBus;
+    public static Socket socketOfServer;
+
+    public static void main(String[] args) {
+        ServerSocket listener = null;
+        serverThreadBus = new ServerThreadBus();
+        System.out.println("Server is waiting to accept user...");
+        int clientNumber = 0;
+        String clientName = "";
+
+        // Mở một ServerSocket tại cổng 7777.
+        // Chú ý bạn không thể chọn cổng nhỏ hơn 1023 nếu không là người dùng
+        // đặc quyền (privileged users (root)).
+        try {
+            listener = new ServerSocket(7777);
+        } catch (IOException e) {
+            System.out.println(e);
+            System.exit(1);
+        }
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                10, // corePoolSize
+                100, // maximumPoolSize
+                10, // thread timeout
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(8) // queueCapacity
+        );
+        try {
+            while (true) {
+                // Chấp nhận một yêu cầu kết nối từ phía Client.
+                // Đồng thời nhận được một đối tượng Socket tại server.
+                socketOfServer = listener.accept();
+                BufferedReader is = new BufferedReader(new InputStreamReader(socketOfServer.getInputStream()));
+                String mess = is.readLine();
+                System.out.println("Message: " + mess);
+                if(mess == null) continue;
+                String[] messArr = mess.split(",");
+                if (messArr[0].equals("connect")) {
+                    clientName = messArr[1];
+                    System.out.println("Client " + clientNumber + " " + clientName + " just connected");
+
+                }
+                ServerThread serverThread = new ServerThread(socketOfServer, clientNumber++, clientName);
+                serverThreadBus.add(serverThread);
+                System.out.println("Số thread đang chạy là: "+serverThreadBus.getLength());
+                executor.execute(serverThread);
+
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                listener.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
---
+++ hotel management/Hotel Management System/src/com/hotelManagementSystem/ChatSys/Server/ServerThread.java
@@ -0,0 +1,90 @@
+package com.hotelManagementSystem.ChatSys.Server;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+/**
+ *
+ * @author Admin
+ */
+public class ServerThread implements Runnable {
+
+    private Socket socketOfServer;
+    private int clientNumber;
+    private String clientName;
+    private BufferedReader is;
+    private BufferedWriter os;
+    private boolean isClosed;
+
+    public BufferedReader getIs() {
+        return is;
+    }
+
+    public BufferedWriter getOs() {
+        return os;
+    }
+
+    public int getClientNumber() {
+        return clientNumber;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public ServerThread(Socket socketOfServer, int clientNumber, String clientName) {
+        this.socketOfServer = socketOfServer;
+        this.clientNumber = clientNumber;
+        this.clientName = clientName;
+        System.out.println("Server thread number " + clientNumber + " " + clientName + " Started");
+        isClosed = false;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // Mở luồng vào ra trên Socket tại Server.
+            is = new BufferedReader(new InputStreamReader(socketOfServer.getInputStream()));
+            os = new BufferedWriter(new OutputStreamWriter(socketOfServer.getOutputStream()));
+            System.out.println("Khời động luông mới thành công, ID là: " + clientNumber + " với tên là: " + clientName);
+            write("get-id" + "," + this.clientNumber + "," + this.clientName);
+            Server.serverThreadBus.sendOnlineList();
+            Server.serverThreadBus.mutilCastSend("global-message"+","+"---Client "+this.clientName+" đã đăng nhập---");
+            String message;
+            while (!isClosed) {
+                message = is.readLine();
+                if (message == null) {
+                    break;
+                }
+                String[] messageSplit = message.split(",");
+                if(messageSplit[0].equals("send-to-global")){
+                    Server.serverThreadBus.boardCast(this.getClientName(),"global-message"+","+"Client "+messageSplit[2]+"-" + messageSplit[3]+": "+messageSplit[1]);
+                }
+                if(messageSplit[0].equals("send-to-person")){// clientNumber, message, clientName, clientNumber loi co the o day
+                    Server.serverThreadBus.sendMessageToPersion(messageSplit[4],"Client "+ messageSplit[2]+"-"+ messageSplit[3]+" (tới bạn): "+messageSplit[1]);
+                }
+            }
+        } catch (IOException e) {
+            isClosed = true;
+            Server.serverThreadBus.remove(clientName);
+            System.out.println(this.clientName+" đã thoát");
+            Server.serverThreadBus.sendOnlineList();
+            Server.serverThreadBus.mutilCastSend("global-message"+","+"---Client "+this.clientName+" đã thoát---");
+        }
+    }
+    public void write(String message) throws IOException{
+        os.write(message);
+        os.newLine();
+        os.flush();
+    }
+}
